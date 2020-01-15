9e2d3c5e52a7569d148c51857582097249a5c76f
David Miller <dlm6693@gmail.com>
2020-01-15 15:21:58 -0500

    created django app and began model creation

--- dark_sky_app/api_data/models.py
+++ dark_sky_app/api_data/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Alerts(models.Model):
+    
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    title = models.CharField(max_length=255)
+    severity = models.CharField(max_length=100)
+    time = models.DateTimeField()
+    expires = models.DateTimeField()
+    description = models.TextField()
+    uri = models.URLField()
+    
+    class Meta:
+        unique_together = ('latitude', 'longitude', 'time', 'expires')
+    
+    def __str__(self):
+        return self.title
+    
+class AlertRegions(models.Model):
+    
+    region = models.CharField(max_length=255)
+    time = models.DateTimeField()
+    expires = models.DateTimeField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    alert = models.ForeignKey(Alerts, on_delete=models.CASCADE)
+    
+    class Meta:
+        unique_together = ('latitude', 'longitude', 'time', 'expires')
