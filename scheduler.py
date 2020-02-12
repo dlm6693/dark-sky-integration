2e66e1121680919a5e86c726a13cb012565b5681
David Miller <dlm6693@gmail.com>
2020-02-12 15:20:15 -0500

    secret key removal

--- scheduler.py
+++ scheduler.py
@@ -4,7 +4,7 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 if __name__ == '__main__':
-    key = 'cab4d327b1b00264becd3f89ea6eb57b'#os.environ.get('DARK_SKY_SECRET_KEY')
+    key = os.environ.get('DARK_SKY_SECRET_KEY')
     dbc = DBConnector(key)
     data = dbc.grab_mapping_data()
     mc = MasterCrawler(key, data)
